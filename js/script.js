3420d4b98b1f4a41e25acd44915d326e246466a6
bryanevan <35902530+bryanevan@users.noreply.github.com>
2023-02-01 18:03:34 -1000

    modified

--- js/script.js
+++ js/script.js
@@ -1,29 +1,26 @@
 function newItem() {
-    
+
     let li = $('<li></li>');
-    let inputValue = $('input').val();
+    let inputValue = $('#input').val();
     li.append(inputValue);
 
-    if (inputValue === '') {
-        alert ("You must write something!");
+    if(inputValue === '') {
+        alert('you must write something!!');
     } else {
         $('#list').append(li);
     }
 
-    function crossOut() {
-        li.toggleClass("strike");
-    }
+    li.on('dblclick', function crossOut() {
+        li.toggleClass('strike');
+    });
 
-        li.on("dblclick", crossOut);
-
-    let crossOutButton = $('<crossOutButton></crossOutButton>');
-    crossOutButton.append(document.createTextNode("X"));
+    let crossOutButton =$('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
     li.append(crossOutButton);
 
-    crossOutButton.on("click", deleteListItem);
-
-    function deleteListItem () {
-        li.addClass("delete");
+    crossOutButton.on('click', deleteListItem);
+    function deleteListItem() {
+        li.addClass('delete')
     }
 
     $('#list').sortable();
